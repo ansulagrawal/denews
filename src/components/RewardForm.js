69e372be2f9d3421ce29a8d01240411c60c38438
Ansul Agrawal <ansulagrawal9@gmail.com>
2022-04-10 22:02:07 +0530

    Change in styling of reward input

--- src/components/RewardForm.js
+++ src/components/RewardForm.js
@@ -1,52 +1,73 @@
-import React, { Component } from 'react'
-import blogUtils from '../utils/blogUtils';
+import React, { Component } from "react";
+import blogUtils from "../utils/blogUtils";
 
 class RewardForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { rewardAmount: 0 };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { rewardAmount: 0 };
+  }
 
-    componentDidMount() {
-        blogUtils.instance.events.RewardSubmission({}, (err, event) => {
-            alert("Success! You just rewarded ", event.returnValues.writer);
-            console.log("EVENT!! Rewarded submission with ID ", event.returnValues.submissionID)
-        })
-    }
+  componentDidMount() {
+    blogUtils.instance.events.RewardSubmission({}, (err, event) => {
+      alert("Success! You just rewarded ", event.returnValues.writer);
+      console.log(
+        "EVENT!! Rewarded submission with ID ",
+        event.returnValues.submissionID
+      );
+    });
+  }
 
-    mySubmitHandler = async () => {
-        console.log("Reacting here")
-        const submissionID = this.props.submissionIndex + 1
-        await blogUtils.getSubmission(submissionID).then(async (sub) => {
-            if (sub.writer === blogUtils.accounts[0]) {
-                alert("You can't reward yourself!");
-            } else {
-                await blogUtils.rewardSubmission(submissionID, this.state.rewardAmount)
-                this.props.fetchSubmissions()
-            }
-        });
+  mySubmitHandler = async () => {
+    if (this.state.rewardAmount > 0) {
+      const submissionID = this.props.submissionIndex + 1;
+      await blogUtils.getSubmission(submissionID).then(async (sub) => {
+        if (sub.writer === blogUtils.accounts[0]) {
+          alert("You can't reward yourself!");
+        } else {
+          await blogUtils.rewardSubmission(
+            submissionID,
+            this.state.rewardAmount
+          );
+          this.props.fetchSubmissions();
+        }
+      });
     }
-    myChangeHandler = (event) => {
-        this.setState({ rewardAmount: event.target.value });
-    }
-    render() {
-        return (
-            <form className="rewardForm" style={{display: "flex", alignItems: "center", justifyContent: "center"}} >
-                <div className="content d-flex justify-content-center pt-5">
-                <div className="primary-input" style={{border: "1px solid black", borderRadius: "14px"}}>
-                    <div className="mb-3 ps-4 ps-xl-6">
-                    <label htmlFor="" className="contact-input">
-                        <input type="number"  onChange={this.myChangeHandler}/>
-                        <button type="button" className="btn" onClick={()=>this.mySubmitHandler()}>
-                        <span>Reward</span>
-                        </button>
-                    </label>
-                    </div>
-                </div>
-                </div>
-            </form>
-        );
-    }
+  };
+  myChangeHandler = (event) => {
+    this.setState({ rewardAmount: event.target.value });
+  };
+  render() {
+    return (
+      <form
+        className="rewardForm"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className="content d-flex justify-content-center pt-5">
+          <div
+            className="primary-input"
+            style={{ border: "1px solid black", borderRadius: "14px" }}
+          >
+            <div className="mb-3 ps-4 ps-xl-6">
+              <label htmlFor="" className="contact-input">
+                <input type="number" onChange={this.myChangeHandler} />
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => this.mySubmitHandler()}
+                >
+                  <span>Reward</span>
+                </button>
+              </label>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default RewardForm;
--- src/components/Submissions.js
+++ src/components/Submissions.js
@@ -1,131 +1,184 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState } from "react";
 import _ from "lodash";
-import RewardForm from './RewardForm'
-import blogUtils from '../utils/blogUtils';
-import Modal from 'react-modal';
+import RewardForm from "./RewardForm";
+import blogUtils from "../utils/blogUtils";
+import Modal from "react-modal";
 
 class Submissions extends Component {
-    state = { submissionsCount: 0, web3: null, accounts: null, contract: null, submissions: [], modalOpen: false };
-    componentDidMount = async () => {
-        try {
-            // Set web3, accounts, and contract to the state, and then proceed with an
-            // example of interacting with the contract's methods.
-            await blogUtils.getVars()
-            const web3 = blogUtils.web3;
-            const accounts = blogUtils.accounts;
-            const instance = blogUtils.instance;
-            if (accounts) {
-                this.setState(
-                    { web3, accounts, contract: instance },
-                    this.fetchSubmissions
-                );
-                // await instance.methods.publishSubmission("content", "title", 0).send({ from: accounts[0], value: web3.utils.toWei("0.05", "ether") });
-                this.state.contract.events.SubmissionEvent({}, (err, event) => {
-                    console.log("EVENT!! Added new submission with ID ", event.returnValues?.submissionID)
-                    this.fetchSubmissions()
-                })
-            }
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-                `Failed to load web3, accounts, or contract. Check console for details.`
-            );
-            console.error(error);
-        }
-    };
-
-    fetchSubmissions = async () => {
-
-        const contract = this.state.contract;
-        let submissions = [];
-        await contract.methods
-            .getSubmissionsLength()
-            .call()
-            .then(res => {
-                this.setState({ submissionsCount: res });
-            }).then(async () => {
-                if (this.state.submissionsCount !== 0) {
-                    await contract.methods
-                        .getAllSubmissions()
-                        .call()
-                        .then(res => {
-                            submissions = res;
-                        });
-                    this.setState({ submissions: submissions });
-                }
-            });
-    };
-    openModal() {
-        this.setState({modalOpen:true});
-    }
-
-    afterOpenModal() {
-    // references are now sync'd and can be accessed.
-        // subtitle.style.color = '#f00';
-    }
-
-    closeModal() {
-        this.setState({modalOpen:false});
-    }
-
-    render() {
-        if (!this.state.accounts) {
-            return <p> Couldn't detect account. Please verify that you have metamask installed and running </p>
-        }
-        if (blogUtils.networkId !== 80001) {
-            return <p>Please connect to the Matic Mumbai network.</p>
-        }
-        let Submissions = [];
-
-        _.forEachRight(this.state.submissions, (value, index) => {
-            let date = new Date(value[3] * 1000)
-            Submissions.push(
-                <div className="submission" key={index} style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
-                    <h2> {value[1]} </h2>{" "}
-                    <pre>{date.toString()}</pre>
-                    <pre>
-                        Written by {value[0]}, Rewards:{" "}
-                        {this.state.web3.utils.fromWei(value["reward"], "ether")} ETH{" "}
-                    </pre>{" "}
-                    <p> {value[2]} </p>{" "}
-                    <button className="auditFormButton" style={{width: "110px"}} onClick={()=>this.setState({modalOpen:true})}>Audit</button>
-                    <RewardForm submissionIndex={index} fetchSubmissions={this.fetchSubmissions} />
-                    
-                </div>
-            );
-        });
-
-        const AuditForm = () => {
-            const [title, setTitle] = useState('');
-            const [description, setDescription] = useState('');
-            const [trustScore, setTrustScore] = useState(0);
-            return(
-                <form style={{display: "flex", flexDirection: "column", width: "70%"}} className="auditForm">
-                    <h1>Submit Audit Report</h1>
-                    <input type="text" placeholder="Title" className="auditInput"/> 
-                    <textarea type="text" placeholder="Description" className="auditInput"/>
-                    <input type="input" placeholder="Trust Score" className="auditInput"/>
-                    <button type="submit" className="auditFormButton" onClick={()=>alert("Audit report submitted! It is being reviewed")}>Submit</button>
-                </form>
-            )
-        }
-        
-        return (
-            <div>
-                <h4>A total of {this.state.submissionsCount} news submission(s) currently exist on the blockchain</h4>
-                {Submissions}
-                <Modal
-                    isOpen={this.state.modalOpen}
-                    onAfterOpen={this.afterOpenModal}
-                    onRequestClose={()=>this.setState({modalOpen:false})}
-                    style={{width: "60vw"}}
-                    contentLabel="Example Modal"
-                >
-                    <AuditForm />
-                </Modal>
-            </div>
+  state = {
+    submissionsCount: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+    submissions: [],
+    modalOpen: false,
+  };
+  componentDidMount = async () => {
+    try {
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      await blogUtils.getVars();
+      const web3 = blogUtils.web3;
+      const accounts = blogUtils.accounts;
+      const instance = blogUtils.instance;
+      if (accounts) {
+        this.setState(
+          { web3, accounts, contract: instance },
+          this.fetchSubmissions
         );
+        // await instance.methods.publishSubmission("content", "title", 0).send({ from: accounts[0], value: web3.utils.toWei("0.05", "ether") });
+        this.state.contract.events.SubmissionEvent({}, (err, event) => {
+          console.log(
+            "EVENT!! Added new submission with ID ",
+            event.returnValues?.submissionID
+          );
+          this.fetchSubmissions();
+        });
+      }
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
     }
+  };
+
+  fetchSubmissions = async () => {
+    const contract = this.state.contract;
+    let submissions = [];
+    await contract.methods
+      .getSubmissionsLength()
+      .call()
+      .then((res) => {
+        this.setState({ submissionsCount: res });
+      })
+      .then(async () => {
+        if (this.state.submissionsCount !== 0) {
+          await contract.methods
+            .getAllSubmissions()
+            .call()
+            .then((res) => {
+              submissions = res;
+            });
+          this.setState({ submissions: submissions });
+        }
+      });
+  };
+  openModal() {
+    this.setState({ modalOpen: true });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalOpen: false });
+  }
+
+  render() {
+    if (!this.state.accounts) {
+      return (
+        <p>
+          {" "}
+          Couldn't detect account. Please verify that you have metamask
+          installed and running{" "}
+        </p>
+      );
+    }
+    if (blogUtils.networkId !== 80001) {
+      return <p>Please connect to the Matic Mumbai network.</p>;
+    }
+    let Submissions = [];
+
+    _.forEachRight(this.state.submissions, (value, index) => {
+      let date = new Date(value[3] * 1000);
+      Submissions.push(
+        <div
+          className="submission"
+          key={index}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <h2> {value[1]} </h2> <pre>{date.toString()}</pre>
+          <pre>
+            Written by {value[0]}, Rewards:{" "}
+            {this.state.web3.utils.fromWei(value["reward"], "ether")} ETH{" "}
+          </pre>{" "}
+          <p> {value[2]} </p>{" "}
+          <button
+            className="auditFormButton"
+            style={{ width: "110px" }}
+            onClick={() => this.setState({ modalOpen: true })}
+          >
+            Audit
+          </button>
+          <RewardForm
+            submissionIndex={index}
+            fetchSubmissions={this.fetchSubmissions}
+          />
+        </div>
+      );
+    });
+
+    const AuditForm = () => {
+      const [title, setTitle] = useState("");
+      const [description, setDescription] = useState("");
+      const [trustScore, setTrustScore] = useState(0);
+      return (
+        <form
+          style={{ display: "flex", flexDirection: "column", width: "70%" }}
+          className="auditForm"
+        >
+          <h1>Submit Audit Report</h1>
+          <input type="text" placeholder="Title" className="auditInput" />
+          <textarea
+            type="text"
+            placeholder="Description"
+            className="auditInput"
+          />
+          <input
+            type="input"
+            placeholder="Trust Score"
+            className="auditInput"
+          />
+          <button
+            type="submit"
+            className="auditFormButton"
+            onClick={() =>
+              alert("Audit report submitted! It is being reviewed")
+            }
+          >
+            Submit
+          </button>
+        </form>
+      );
+    };
+
+    return (
+      <div>
+        <h4>
+          A total of {this.state.submissionsCount} news submission(s) currently
+          exist on the blockchain
+        </h4>
+        {Submissions}
+        <Modal
+          isOpen={this.state.modalOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={() => this.setState({ modalOpen: false })}
+          style={{ width: "60vw" }}
+          contentLabel="Example Modal"
+        >
+          <AuditForm />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default Submissions;
